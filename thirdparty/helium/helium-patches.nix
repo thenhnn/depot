66c68e3ead3a92e76772857bc77dc8a52ed49917
copybara sync <git@nhnn.dev>
2026-05-07 14:26:45 +0300

    Project import generated by Copybara.
    GitOrigin-RevId: c1c056adfbca059a1a8308e13e0a32faf8bfbf03

--- thirdparty/helium/helium-patches.nix
+++ thirdparty/helium/helium-patches.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}:
 pkgs.stdenv.mkDerivation {
   pname = "helium-patches";
-  version = "0.11.7";
+  version = "0.12.1";
   src = pkgs.fetchFromGitHub {
     owner = "imputnet";
     repo = "helium";
-    rev = "7330d5770912745a3a208c19c86d75ff42081a69";
-    hash = "sha256-87sgs1iv30eD7vRfSV7iMUoz/yKU26Z/1gD27zSA+UU=";
+    rev = "4f69833a0142f2243c44ea38d0b06eb265d61cab";
+    hash = "sha256-KGBDlnSG26h/cl0y13zP+0d22JjFlHfqrJDxwrs7I04=";
   };
 
   dontBuild = true;
